55197916320bf5dfb0cf6fcfebf3cf1a82daab2e
kagchi <yonuxbot@gmail.com>
2023-05-03 20:00:25 +0700

    feat: use custom epoch

--- snowflake/snowflake.go
+++ snowflake/snowflake.go
@@ -7,7 +7,13 @@ import (
 )
 
 func GenerateNew() string {
-	id := snowflake.New(time.Now())
+	epoch, err := time.Parse(time.RFC3339Nano, "2019-08-28T07:17:29.593Z")
+
+	if err != nil {
+		panic(err)
+	}
+
+	id := snowflake.ID((time.Now().UnixMilli() - epoch.Unix()) << 22)
 
 	return strconv.FormatUint(uint64(id), 10)
 }
